4f995f56d7b82a830636755bf0fd375160af8974
Sneha Endait <snehaendait@gmail.com>
2017-10-21 20:12:37 -0400

    Integrated the game and the client-server

---
+++ connection.py
@@ -0,0 +1,17 @@
+EOM = "::"
+
+
+class Connection:
+    def __init__(self, sock):
+        self.data = ""
+        self.sock = sock
+
+    def read(self):
+        if not EOM in self.data:
+            self.data += self.sock.recv(1024).decode('utf-8')
+
+        msg, self.data = self.data.split(EOM, 1)
+        return msg
+
+    def send(self, msg):
+        self.sock.sendall((msg + EOM).encode('utf-8'))
